2e19218f2dca77e2f39b360670a2b7274badb124
Manu <manuel.brusche@web.de>
2023-10-29 19:22:49 +0100

    added both windows

---
+++ Combat-Manager/Windows/NpcsWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Combat_Manager
+{
+    public partial class NpcsWindow : Form
+    {
+        public NpcsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Combat-Manager/Windows/PlayersWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Combat_Manager
+{
+    public partial class PlayersWindow : Form
+    {
+        public PlayersWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
