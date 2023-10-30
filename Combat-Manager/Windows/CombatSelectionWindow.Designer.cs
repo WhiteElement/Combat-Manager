4e824208a1b5ba1958c52cbd3e9bb57f42cab7c7
Manu <manuel.brusche@web.de>
2023-10-30 08:04:47 +0100

    -added box for players

---
+++ Combat-Manager/Windows/CombatSelectionWindow.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace Combat_Manager
+{
+    partial class CombatSelectionWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // playerBox
+            // 
+            this.playerBox.FormattingEnabled = true;
+            this.playerBox.Location = new System.Drawing.Point(40, 61);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(256, 304);
+            this.playerBox.TabIndex = 0;
+            // 
+            // CombatSelectionWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playerBox);
+            this.Name = "CombatSelectionWindow";
+            this.Text = "CombatSelectionWindow";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.CheckedListBox playerBox;
+
+        #endregion
+    }
+}
---
+++ Combat-Manager/Windows/CombatSelectionWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+using Combat_Manager.Models;
+using Combat_Manager.Services;
+
+namespace Combat_Manager
+{
+    public partial class CombatSelectionWindow : Form
+    {
+        private readonly PlayerService _playerService;
+        public CombatSelectionWindow()
+        {
+            InitializeComponent();
+            _playerService = new PlayerService();
+            PopulateCheckedListBoxes();
+        }
+
+        private void PopulateCheckedListBoxes()
+        {
+            var allPlayers = _playerService.LoadPlayersFromFile();
+            foreach (Player player in allPlayers)
+                playerBox.Items.Add(player.Name);
+        }
+    }
+}
